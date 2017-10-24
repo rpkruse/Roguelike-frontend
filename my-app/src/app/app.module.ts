678b0baca1fe2409e4009ed11ea901751e03b7fe
Ryan Kruse <rpkruse@ksu.edu>
2017-10-24 09:01:53 -0500

    Added bootstrap to make life easier

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
