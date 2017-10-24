bd8d2c73250d88783eebdd20bac28f655c47d54b
Ryan Kruse <rpkruse@ksu.edu>
2017-10-24 10:49:21 -0500

    Tables for home page look nicer, started some logic

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -9,4 +9,19 @@ import 'material-design-lite/material.js';
 })
 export class HomeComponent{
 
+  /*
+    Will pull the last page of the list from the DB for top_n to display
+    @param clicked: string - Either: TOP_PLAYERS, TOP_KILLS, or TOP_LEVEL
+  */
+  private previous(clicked: string){
+    console.log("Previous clicked for: " + clicked);
+  }
+
+  /*
+    Will pull the next page of the list from the DB for top_n to display
+    @param clicked: string - Either: TOP_PLAYERS, TOP_KILLS, or TOP_LEVEL
+  */
+  private next(clicked: string){
+    console.log("Next clicked for: " + clicked);
+  }
 }
